c949a4362b72e900723c943525cb55f96a9147d3
CESchnell <60896893+CESchnell@users.noreply.github.com>
2020-04-29 03:15:49 -0400

    Add files via upload

---
+++ data/FenceBetweenLinnentownandNearbyWhiteResidents_6.js
@@ -0,0 +1,8 @@
+var json_FenceBetweenLinnentownandNearbyWhiteResidents_6 = {
+"type": "FeatureCollection",
+"name": "FenceBetweenLinnentownandNearbyWhiteResidents_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1" }, "geometry": { "type": "LineString", "coordinates": [ [ -83.380365029647336, 33.948161486051063 ], [ -83.380841978224382, 33.947968006911317 ] ] } }
+]
+}
---
+++ data/GatheringPlaces_7.js
@@ -0,0 +1,11 @@
+var json_GatheringPlaces_7 = {
+"type": "FeatureCollection",
+"name": "GatheringPlaces_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Name": "Edward's Grocery Store", "Type": "Store", "Black_only": "yes" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -83.381317024304963, 33.950952160961421 ], [ -83.381286228627971, 33.950854174716454 ], [ -83.381202240417991, 33.950870972358452 ], [ -83.381233036094983, 33.950954960568424 ], [ -83.381317024304963, 33.950952160961421 ] ] ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Name": "Donut Shop", "Type": "Donut Shop", "Black_only": "yes" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -83.379680513807372, 33.949272123698449 ], [ -83.379694511842359, 33.94916573863248 ], [ -83.379585327169394, 33.949176937060479 ], [ -83.379571329134407, 33.949260925270451 ], [ -83.379680513807372, 33.949272123698449 ] ] ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Name": "Patricuk Hue Mell's Home", "Type": "Renter's place", "Black_only": "no" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -83.386794315192077, 33.943210974545359 ], [ -83.386603941916135, 33.943194176903361 ], [ -83.386589943881134, 33.943362153323307 ], [ -83.386769118729077, 33.943353754502311 ], [ -83.386794315192077, 33.943210974545359 ] ] ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Name": "Jeruel Academy\/Union Institute ", "Type": null, "Black_only": null }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -83.382117453135209, 33.950255525724344 ], [ -83.38210190596088, 33.950079509500654 ], [ -83.381826498872712, 33.950107827568189 ], [ -83.381845377584398, 33.950283288535651 ], [ -83.382117453135209, 33.950255525724344 ] ] ] } }
+]
+}
